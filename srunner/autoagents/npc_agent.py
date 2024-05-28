776ea327165e0374d9486c68d9747134a29dd7c1
dunesq <hangq@ucr.edu>
2024-05-27 22:22:52 -0700

    town10

---
+++ srunner/autoagents/npc_agent.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+
+"""
+This module provides an NPC agent to control the ego vehicle
+"""
+
+from __future__ import print_function
+
+import carla
+import pygame
+from agents.navigation.basic_agent import BasicAgent
+
+from srunner.autoagents.autonomous_agent import AutonomousAgent
+from srunner.scenariomanager.carla_data_provider import CarlaDataProvider
+
+
+class HumanInterface(object):
+
+    """
+    Class to control a vehicle manually for debugging purposes
+    """
+
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+        self._surface = None
+
+        pygame.init()
+        pygame.font.init()
+        self._clock = pygame.time.Clock()
+        self._display = pygame.display.set_mode((self._width, self._height), pygame.HWSURFACE | pygame.DOUBLEBUF)
+        pygame.display.set_caption("Human Agent")
+
+    def run_interface(self, input_data):
+        """
+        Run the GUI
+        """
+        # process sensor data
+        image_center = input_data['Left'][1][:, :, -2::-1]
+
+        # display image
+        self._surface = pygame.surfarray.make_surface(image_center.swapaxes(0, 1))
+        if self._surface is not None:
+            self._display.blit(self._surface, (0, 0))
+        pygame.display.flip()
+
+    def quit_interface(self):
+        """
+        Stops the pygame window
+        """
+        pygame.quit()
+
+
+class NpcAgent(AutonomousAgent):
+
+    """
+    NPC autonomous agent to control the ego vehicle
+    """
+
+    _agent = None
+    _route_assigned = False
+
+    def setup(self, path_to_conf_file):
+        """
+        Setup the agent parameters
+        """
+        self._agent = None
+        self.camera_width = 800
+        self.camera_height = 500
+
+        self._hic = HumanInterface(self.camera_width, self.camera_height)
+
+    def sensors(self):
+        """
+        Define the sensor suite required by the agent
+
+        :return: a list containing the required sensors in the following format:
+
+        [
+            {'type': 'sensor.camera.rgb', 'x': 0.7, 'y': -0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0, 'yaw': 0.0,
+                      'width': 300, 'height': 200, 'fov': 100, 'id': 'Left'},
+
+            {'type': 'sensor.camera.rgb', 'x': 0.7, 'y': 0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0, 'yaw': 0.0,
+                      'width': 300, 'height': 200, 'fov': 100, 'id': 'Right'},
+
+            {'type': 'sensor.lidar.ray_cast', 'x': 0.7, 'y': 0.0, 'z': 1.60, 'yaw': 0.0, 'pitch': 0.0, 'roll': 0.0,
+             'id': 'LIDAR'}
+        ]
+        """
+
+        sensors = [
+            {'type': 'sensor.camera.rgb', 'x': 0.7, 'y': -0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0, 'yaw': 0.0,
+             'width': self.camera_width, 'height': self.camera_height, 'fov': 100, 'id': 'Left'},
+        ]
+
+        return sensors
+
+    def run_step(self, input_data, timestamp):
+        self._hic.run_interface(input_data)
+        # Execute one step of navigation.
+        if not self._agent:
+
+            # Search for the ego actor
+            hero_actor = None
+            for actor in CarlaDataProvider.get_world().get_actors():
+                if 'role_name' in actor.attributes and actor.attributes['role_name'] == 'hero':
+                    hero_actor = actor
+                    break
+
+            if not hero_actor:
+                return carla.VehicleControl()
+
+            # Add an agent that follows the route to the ego
+            self._agent = BasicAgent(hero_actor, 30)
+
+            plan = []
+            prev_wp = None
+            for transform, _ in self._global_plan_world_coord:
+                wp = CarlaDataProvider.get_map().get_waypoint(transform.location)
+                if prev_wp:
+                    plan.extend(self._agent.trace_route(prev_wp, wp))
+                prev_wp = wp
+
+            self._agent.set_global_plan(plan)
+
+            return carla.VehicleControl()
+
+        else:
+            return self._agent.run_step()
+        
+    def destroy(self):
+        """
+        Cleanup
+        """
+        self._hic.quit_interface = True
+        
+        
